5c54d7f81bc2dfa75371b92c62c5bafe679e6003
616764899 <a616764899@126.com>
2017-04-21 21:59:28 +0800

    1、完成新歌榜Ui

--- app/src/main/java/com/meimei/meimusic/module/main/rankinglist/NewMusic/NewMusicActivity.java
+++ app/src/main/java/com/meimei/meimusic/module/main/rankinglist/NewMusic/NewMusicActivity.java
@@ -2,6 +2,8 @@ package com.meimei.meimusic.module.main.rankinglist.newmusic;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.meimei.meimusic.MyApplication;
 import com.meimei.meimusic.R;
 import com.meimei.meimusic.base.view.BaseActivity;
 import com.meimei.meimusic.entity.RankingList;
@@ -35,12 +38,14 @@ public class NewMusicActivity extends BaseActivity implements INewMusicView{
     @BindView(R.id.tv_title_toolbar_ranking_official)
     TextView mTitle;
     @BindView(R.id.relativelayout_ranking_official_header)
-    RelativeLayout mHeaderLayout;
-    @BindView(R.id.framelayout_ranking_official)
-    FrameLayout mFrameLayout;
+    RelativeLayout mHeaderLayoutText;
+    @BindView(R.id.framelayout_ranking_official_header)
+    FrameLayout mHeaderLayout;
+    @BindView(R.id.recyc_ranking_official)
+    RecyclerView mRecycNewMusic;
     @BindView(R.id.linearlayout_loading)
     LinearLayout mLoading;
-    @BindView(R.id.tv_neterror_ranking_official)
+    @BindView(R.id.tv_neterror_loading)
     TextView mTvNetError;
     @BindView(R.id.image_song_loading)
     ImageView mImageLoading;
@@ -50,10 +55,10 @@ public class NewMusicActivity extends BaseActivity implements INewMusicView{
 
     private int mStatusHeight;
 
-    private RecyclerView mRecycNewMusic;
-    private NewMusicAdapter mAdapter;
-
     private NewMusicPresenter mPresenter;
+
+    private NewMusicAdapter mAdapter;
+    private NestedScrollView mScrollView;
 
     @Override
     protected void initFragment() {
@@ -69,39 +74,44 @@ public class NewMusicActivity extends BaseActivity implements INewMusicView{
     }
 
     private void initOther() {
+        mScrollView = (NestedScrollView) findViewById(R.id.scrollview_ranking_official);
+
         mStatusHeight = DensityUtil.getStatusHeight(this);
         mPresenter = new NewMusicPresenter(this);
 
-        mHeaderLayout.setBackground(new ColorDrawable(getResources().getColor(R.color.relativelayout_bg_official_header_new_music)));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mHeaderLayout.setBackground(new ColorDrawable(getResources().getColor(R.color.relativelayout_bg_official_header_new_music)));
+        }
 
-        mPresenter.requestNewMusicForNet(requestNum);
+        mScrollView.setOnScrollChangeListener(onScrollChangeListener);
     }
 
     private void initToolbar() {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.toolbar_bg_official_new_music)));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.toolbar_bg_transparent_official_new_music)));
         mToolbar.setPadding(0,mStatusHeight,0,0);
 
     }
 
     private void initRecyclerView() {
-        mRecycNewMusic = new RecyclerView(this);
+
         mAdapter = new NewMusicAdapter();
         mRecycNewMusic.setNestedScrollingEnabled(false);
         mRecycNewMusic.setLayoutManager(new LinearLayoutManager(this));
         mRecycNewMusic.setAdapter(mAdapter);
+
     }
 
     private void initData() {
-
+        mPresenter.requestNewMusicForNet(requestNum);
     }
 
     @Override
     public void showNewMusicList(List<RankingList.songList> songlist) {
 
-        mFrameLayout.addView(mRecycNewMusic);
-        boolean showDataSuccess = mAdapter.loadData(songlist);
+        mAdapter.addData(null);
+        boolean showDataSuccess = mAdapter.addDataList(songlist);
 
         LogUtil.i(TAG,"showDataSuccess" + showDataSuccess);
     }
@@ -124,7 +134,25 @@ public class NewMusicActivity extends BaseActivity implements INewMusicView{
         mTvNetError.setVisibility(View.GONE);
     }
 
+    private NestedScrollView.OnScrollChangeListener onScrollChangeListener = new NestedScrollView.OnScrollChangeListener() {
+        @Override
+        public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+            int flag = DensityUtil.dp2px(MyApplication.getContext(),70);
 
+            if (scrollY > flag && scrollY < (mHeaderLayout.getHeight() - mToolbar.getHeight())){
+                mTitle.setText("云音乐新歌榜");
+            }
+
+            if (scrollY < flag){
+                mTitle.setText("");
+            }
+
+            float TransParentScale = (float) scrollY / (mHeaderLayout.getHeight() - mToolbar.getHeight());  //透明度比例
+
+            mToolbar.getBackground().setAlpha(Math.min((int) (TransParentScale * 255),255));
+            mHeaderLayoutText.setAlpha(1f - TransParentScale);
+        }
+    };
 
     @OnClick(R.id.image_back_toolbar_ranking_official)
     void onBack1(){
--- app/src/main/java/com/meimei/meimusic/module/main/rankinglist/NewMusic/NewMusicAdapter.java
+++ app/src/main/java/com/meimei/meimusic/module/main/rankinglist/NewMusic/NewMusicAdapter.java
@@ -4,6 +4,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.meimei.meimusic.R;
@@ -18,27 +21,56 @@ import butterknife.ButterKnife;
  */
 public class NewMusicAdapter extends BaseAdapter<RankingList.songList>{
 
+    private final int ITEM_HEADER = 0;
+    private final int ITEM_NORMAL = 1;
+
     @Override
     protected RecyclerView.ViewHolder createHolder(ViewGroup parent, int viewType) {
-        return new NewMusicViewHolder(LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.item_recyc_ranking_new_music,parent,false));
+        if (viewType == ITEM_HEADER){
+            return new NewMusicHeaderViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_recyc_header_ranking_official,parent,false));
+        }else {
+            return new NewMusicViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_recyc_ranking_official,parent,false));
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        NewMusicViewHolder viewHolder = (NewMusicViewHolder) holder;
 
-        viewHolder.setData(getDataController().getData(position),position);
+        if (holder instanceof NewMusicViewHolder){
+
+            NewMusicViewHolder viewHolder = (NewMusicViewHolder) holder;
+            viewHolder.setData(getDataController().getData(position),position);
+
+        }
+
+        if (holder instanceof NewMusicHeaderViewHolder){
+
+            NewMusicHeaderViewHolder viewHolder = (NewMusicHeaderViewHolder) holder;
+            viewHolder.setData(getDataController().getDataSize() - 1);  //j减掉头Item
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0){
+            return ITEM_HEADER;
+        }else {
+            return ITEM_NORMAL;
+        }
     }
 
     public static class NewMusicViewHolder extends RecyclerView.ViewHolder{
 
-        @BindView(R.id.tv_item_ranking_new_music_num)
+        @BindView(R.id.tv_item_ranking_official_num)
         TextView mNum;
-        @BindView(R.id.tv_item_ranking_new_music_name)
+        @BindView(R.id.tv_item_ranking_official_name)
         TextView mName;
-        @BindView(R.id.tv_item_ranking_new_music_author)
+        @BindView(R.id.tv_item_ranking_official_author)
         TextView mAuthor;
+        @BindView(R.id.image_item_ranking_official_more)
+        ImageView mMoreInfo;
 
         public NewMusicViewHolder(View itemView) {
             super(itemView);
@@ -46,10 +78,32 @@ public class NewMusicAdapter extends BaseAdapter<RankingList.songList>{
         }
 
         private void setData(RankingList.songList songInfo,int position){
-            mNum.setText(position + "");
+            mNum.setText((position) + "");
             mName.setText(songInfo.title);
-            mAuthor.setText(songInfo.author);
+            mAuthor.setText(songInfo.author + " - " + songInfo.title);
+        }
+    }
+
+    public static class NewMusicHeaderViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.relativelayout_playall_ranking_official)
+        RelativeLayout mPlayAll;
+        @BindView(R.id.linearlayout_more_choice_ranking_official)
+        LinearLayout mMoreChoice;
+        @BindView(R.id.tv_songnum_ranking_official)
+        TextView mSongNum;
+
+        public NewMusicHeaderViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
         }
 
+        private void setData(int songNum){
+            mSongNum.setText("(" + songNum + ")");
+        }
+
+        private void setOnClick(){
+
+        }
     }
 }
